5136177ac267bb76dfee5c1beacaf9ddc1708daa
SuperAnna-Lianna <anna.lan@outlook.com>
2016-07-29 18:00:20 +0800

    从尾到头打印链表

--- src/SolutionLianBiao.java
+++ src/SolutionLianBiao.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 //输入一个链表，从尾到头打印链表每个节点的值。
 public class SolutionLianBiao {
-    
+//解题思路:  链表中的属性:本身,和他下一个结点
+//         1.栈的形式(先进后出)   2.递归:  每次都去找结点的next结点,先保存后面的结点再保存自己的结点
+//递归特点:重复执行,跳出重复的条件
           public class ListNode {
               int val;
               ListNode next = null;
@@ -23,6 +25,3 @@ public class SolutionLianBiao {
             return list;
         }
 }
-//解题思路:  链表中的属性:本身,和他下一个结点
-//         1.栈的形式(先进后出)   2.递归:  每次都去找结点的next结点,先保存后面的结点再保存自己的结点
-//递归特点:重复执行,跳出重复的条件
