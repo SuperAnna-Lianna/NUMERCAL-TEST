0374f42740a990f142a0858880eba88cfb48d630
SuperAnna-Lianna <anna.lan@outlook.com>
2016-07-30 21:29:34 +0800

    斐波那契数列

--- src/SolutionShuLie.java
+++ src/SolutionShuLie.java
@@ -1,4 +1,8 @@
 public class SolutionShuLie {
+//大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
+//1.递归  n(0-开始)    n>1 时fn=f(n-1)+f(n-2)
+//2.递归效率低  有大量的重复计算
+//3.采用循环  f2=f0+f1   f0=f1  f1=f2  return f2;
     public int Fibonacci(int n) {
        int one=0;
        int two=1;
@@ -14,7 +18,3 @@ public class SolutionShuLie {
         return sumleft;
     }
 }
-//大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项。
-//1.递归  n(0-开始)    n>1 时fn=f(n-1)+f(n-2)
-//2.递归效率低  有大量的重复计算
-//3.采用循环  f2=f0+f1   f0=f1  f1=f2  return f2;
